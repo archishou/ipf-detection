b7529c62015459255e28dc651720e9627f5ae5fb
Archishmaan Peyyety <archishou@gmail.com>
2020-03-05 02:49:31 -0500

    inital commit: train: 68 test: 88
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

---
+++ python/test.py
@@ -0,0 +1,141 @@
+# Load various imports
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+from keras.callbacks import ModelCheckpoint
+from datetime import datetime
+import os
+import librosa
+import librosa.display
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from sklearn import metrics
+
+from python.helpers.wavhelper import WavFileHelper
+
+data_set = "/Users/Archish/Documents/CodeProjects/Python/IPF/datafiles/all_data"
+
+
+def main():
+    features = []
+    for file in os.listdir(data_set):
+        if file.endswith(".wav"):
+            #print(file)
+            class_label = class_name(file)
+            data_file = os.path.join(data_set, file)
+            data = extract_features(data_file)
+            features.append([data, class_label])
+
+    # Convert into a Panda dataframe
+    featuresdf = pd.DataFrame(features, columns=['feature', 'class_label'])
+
+    print('Finished feature extraction from ', len(featuresdf), ' files')
+
+    x = np.array(featuresdf.feature.tolist())
+    y = np.array(featuresdf.class_label.tolist())
+
+    # Encode the classification labels
+    le = LabelEncoder()
+    yy = to_categorical(le.fit_transform(y))
+
+    # split the dataset
+    x_train, x_test, y_train, y_test = train_test_split(x, yy, test_size=0.2, random_state=42)
+    """
+    num_rows = 40
+    num_columns = 174
+    """
+    num_rows = 5
+    num_columns = 8
+    num_channels = 1
+
+    # print(x_train.shape)
+    x_train = x_train.reshape(x_train.shape[0], num_rows, num_columns, num_channels)
+    x_test = x_test.reshape(x_test.shape[0], num_rows, num_columns, num_channels)
+
+    num_labels = yy.shape[1]
+    filter_size = 2
+
+    # Construct model
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=2, input_shape=(num_rows, num_columns, num_channels), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+    """
+    model.add(Conv2D(filters=32, kernel_size=2, activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+    
+    model.add(Conv2D(filters=64, kernel_size=2, activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(filters=128, kernel_size=2, activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+    
+    """
+    model.add(GlobalAveragePooling2D())
+
+    model.add(Dense(num_labels, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+    # Display model architecture summary
+    model.summary()
+
+    # Calculate pre-training accuracy
+    score = model.evaluate(x_test, y_test, verbose=1)
+    accuracy = 100 * score[1]
+
+    print("Pre-training accuracy: %.4f%%" % accuracy)
+
+    num_epochs = 72
+    num_batch_size = 256
+
+    checkpointer = ModelCheckpoint(filepath='weights.best.basic_cnn.hdf5',
+                                   verbose=1, save_best_only=True)
+    start = datetime.now()
+
+    model.fit(x_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(x_test, y_test),
+              callbacks=[checkpointer], verbose=1)
+
+    duration = datetime.now() - start
+    print("Training completed in time: ", duration)
+
+    # Evaluating the model on the training and testing set
+    score = model.evaluate(x_train, y_train, verbose=0)
+    print("Training Accuracy: ", score[1])
+
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print("Testing Accuracy: ", score[1])
+
+
+def extract_features(file_name):
+    try:
+        audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        mfccsscaled = np.mean(mfccs.T, axis=0)
+
+    except Exception as e:
+        print("Error encountered while parsing file: ", file_name)
+        return None
+
+    return mfccsscaled
+
+
+def class_name(file):
+    if file.startswith("ipf"):
+        return "ipf"
+    if file.startswith("healthy"):
+        return "not_ipf"
+    if file.startswith("copd"):
+        return "not_ipf"
+if __name__ == '__main__':
+    main()
