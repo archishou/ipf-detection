567d8375c896e3846558e622273d6f52b64cc18e
Archishmaan Peyyety <archishou@gmail.com>
2020-03-06 21:08:59 -0500

    test model
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- python/test_model.py
+++ python/test_model.py
@@ -2,7 +2,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 import pandas as pd
-import numpy as np
 from keras.callbacks import ModelCheckpoint
 from datetime import datetime
 import os
@@ -10,30 +9,86 @@ import librosa
 import librosa.display
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.optimizers import Adam
-from keras.utils import np_utils
-from sklearn import metrics
+from keras.layers import Dense, Dropout
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
 
-from python.helpers.wavhelper import WavFileHelper
-
-path = '/Users/Archish/Documents/CodeProjects/Python/IPF/python/saved_models/weights.best.basic_cnn.hdf5'
+data_set = "/Users/Archish/Documents/CodeProjects/Python/IPF/datafiles/all_data"
+model_path = "/Users/Archish/Documents/CodeProjects/Python/IPF/python/saved_models/weights.best.cnn.hdf5"
 
 
-def create_model():
+def main():
+    features = []
+    for file in os.listdir(data_set):
+        if file.endswith(".wav"):
+            # print(file)
+            class_label = class_name(file)
+            data_file = os.path.join(data_set, file)
+            data = extract_features(data_file)
+            features.append([data, class_label])
+
+    # Convert into a Panda dataframe
+    featuresdf = pd.DataFrame(features, columns=['feature', 'class_label'])
+
+    print('Finished feature extraction from ', len(featuresdf), ' files')
+
+    x = np.array(featuresdf.feature.tolist())
+    y = np.array(featuresdf.class_label.tolist())
+
+    # Encode the classification labels
+    le = LabelEncoder()
+    yy = to_categorical(le.fit_transform(y))
+
+    # split the dataset
+    num_rows = 40
+    num_columns = 174
+    num_channels = 1
+
+    num_labels = yy.shape[1]
+
+    # Construct modelas
     model = Sequential()
-    model.add(Conv2D(filters=16, kernel_size=2, input_shape=(40, 174, 1), activation='relu'))
+    model.add(Conv2D(filters=16, kernel_size=2, input_shape=(num_rows, num_columns, num_channels), activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
     model.add(Dropout(0.2))
 
     model.add(Conv2D(filters=32, kernel_size=2, activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
     model.add(Dropout(0.2))
+
     model.add(GlobalAveragePooling2D())
 
-    model.add(Dense(3, activation='softmax'))
-    return model
+    model.add(Dense(num_labels, activation='softmax'))
+
+    # Display model architecture summary
+    model.summary()
+
+    model.load_weights(model_path)
+
+    while True:
+        file = input("choose a file: ") + ".wav"
+
+        prediction_feature = extract_features(os.path.join(data_set, file))
+        prediction_feature = prediction_feature.reshape(1, num_rows, num_columns, num_channels)
+
+        predicted_vector = model.predict_classes(prediction_feature)
+        predicted_class = le.inverse_transform(predicted_vector)
+        print("The predicted class is:", predicted_class[0], '\n')
+
+        predicted_proba_vector = model.predict_proba(prediction_feature)
+        predicted_proba = predicted_proba_vector[0]
+        for i in range(len(predicted_proba)):
+            category = le.inverse_transform(np.array([i]))
+            print(category[0], "\t\t : ", format(predicted_proba[i], '.32f'))
+
+
+def class_name(file):
+    if file.startswith("ipf"):
+        return "ipf"
+    if file.startswith("healthy"):
+        return "not_ipf"
+    if file.startswith("copd"):
+        return "not_ipf"
+
 
 def extract_features(file_name):
     max_pad_len = 174
@@ -49,27 +104,5 @@ def extract_features(file_name):
 
     return mfccs
 
-def load_trained_model(weights_path):
-   model = create_model()
-   model.load_weights(weights_path)
-   return model
-
-
-def main():
-    model = load_trained_model(path)
-    file = input("enter file path: ")
-    file = os.path.join('/Users/Archish/Documents/CodeProjects/Python/IPF/datafiles/all_data/', file)
-    prediction_feature = extract_features(file)
-    prediction_feature = prediction_feature.reshape(1, 40, 174, 1)
-    le = LabelEncoder()
-    predicted_vector = model.predict_classes(prediction_feature)
-    predicted_class = le.inverse_transform(predicted_vector)
-    print("The predicted class is:", predicted_class[0], '\n')
-
-    predicted_proba_vector = model.predict_proba(prediction_feature)
-    predicted_proba = predicted_proba_vector[0]
-    for i in range(len(predicted_proba)):
-        category = le.inverse_transform(np.array([i]))
-        print(category[0], "\t\t : ", format(predicted_proba[i], '.32f'))
 if __name__ == '__main__':
     main()
